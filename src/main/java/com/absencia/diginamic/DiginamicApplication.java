623cf08f10fa166b094b1d5b5a06908b5de1dbae
Mattéo <matteo@studiokg.fr>
2024-03-18 11:58:19 +0100

    Initial commit

---
+++ src/main/java/com/absencia/diginamic/DiginamicApplication.java
@@ -0,0 +1,15 @@
+package com.absencia.diginamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiginamicApplication {
+
+	public static void main(String[] args) {
+		System.err.println("coucou");
+		SpringApplication.run(DiginamicApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/absencia/diginamic/DiginamicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absencia.diginamic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiginamicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
