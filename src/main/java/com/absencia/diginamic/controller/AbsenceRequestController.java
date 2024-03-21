7f1457fade651fef311028480f5dafaeaa3f3d00
Clement Olivier <oliclems@gmail.com>
2024-03-21 15:24:43 +0100

    fix(absences-requests): si userId n'existe pas alors renvoie tableau vide

--- src/main/java/com/absencia/diginamic/controller/AbsenceRequestController.java
+++ src/main/java/com/absencia/diginamic/controller/AbsenceRequestController.java
@@ -1,11 +1,8 @@
 package com.absencia.diginamic.controller;
 
 import com.absencia.diginamic.dto.AbsenceRequestResponse;
-import com.absencia.diginamic.model.Absence;
-import com.absencia.diginamic.model.AbsenceRequestStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.absencia.diginamic.model.AbsenceRequest;
 import com.absencia.diginamic.service.AbsenceRequestService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -30,7 +27,7 @@ public class AbsenceRequestController {
     public ResponseEntity<List<AbsenceRequestResponse>> getAbsencesByUserId(@PathVariable Long userId) {
         if (!absenceRequestService.userExists(userId)) {
             logger.error("Utilisateur non trouvé avec l'ID : {}", userId);
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.ok(Collections.emptyList());
         }
 
         List<AbsenceRequestResponse> absencesRequests  = absenceRequestService.getAbsencesByUserId(userId);
