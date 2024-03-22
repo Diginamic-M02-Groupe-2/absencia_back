b0db0dd146d8e2d4f7c19cb7aa05cf121c649225
Clement Olivier <oliclems@gmail.com>
2024-03-22 10:53:16 +0100

    fix(requests): remove unused userService from absenceRequests

--- src/main/java/com/absencia/diginamic/service/AbsenceRequestServiceImpl.java
+++ src/main/java/com/absencia/diginamic/service/AbsenceRequestServiceImpl.java
@@ -2,7 +2,6 @@ package com.absencia.diginamic.service;
 
 import com.absencia.diginamic.model.AbsenceRequest;
 import com.absencia.diginamic.repository.AbsenceRequestRepository;
-import com.absencia.diginamic.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +12,6 @@ public class AbsenceRequestServiceImpl implements AbsenceRequestService {
 
     @Autowired
     private AbsenceRequestRepository absenceRequestRepository;
-
-    @Autowired
-    private UserRepository userRepository;
 
     @Override
     public List<AbsenceRequest> getAbsencesByUserId(Long userId) {
