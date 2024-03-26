1a570131a08d7d9c93320232546c5cc33d9d7d5e
matteokeole <matteo@keole.net>
2024-03-26 18:57:09 +0100

    refactor: Allowed HTTP methods

--- src/main/java/com/absencia/diginamic/config/WebSecurityConfig.java
+++ src/main/java/com/absencia/diginamic/config/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.absencia.diginamic.config;
 
 import com.absencia.diginamic.service.UserService;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -50,8 +52,7 @@ public class WebSecurityConfig {
 				final CorsConfiguration corsConfiguration = new CorsConfiguration();
 
 				corsConfiguration.applyPermitDefaultValues();
-				corsConfiguration.addAllowedMethod("PATCH");
-				corsConfiguration.addAllowedMethod("DELETE");
+				corsConfiguration.setAllowedMethods(List.of("PATCH", "DELETE"));
 
 				return corsConfiguration;
 			}))
