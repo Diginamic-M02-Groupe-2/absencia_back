25b0945c6ecc8e28abbebb296d08527ddab1703f
Clement Olivier <oliclems@gmail.com>
2024-03-28 09:57:30 +0100

    fix(emplyerWtr): fix sql request issue

--- src/main/java/com/absencia/diginamic/entity/EmployerWtr.java
+++ src/main/java/com/absencia/diginamic/entity/EmployerWtr.java
@@ -33,7 +33,7 @@ import java.time.LocalDate;
 )
 @NamedQuery(
 		name = "EmployerWtr.isDateConflictingWithOther",
-		query = "SELECT COUNT(ew) > 0 FROM EmployerWtr ew WHERE ew.id <> :id AND ew.date = :date AND ew.deletedAT IS NULL"
+		query = "SELECT COUNT(ew) > 0 FROM EmployerWtr ew WHERE ew.id <> :id AND ew.date = :date AND ew.deletedAt IS NULL"
 )
 public class EmployerWtr {
 	@Id
