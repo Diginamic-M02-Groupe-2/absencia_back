4707a0e088b253c6280d5a66e2be28b041b3d563
Clement Olivier <oliclems@gmail.com>
2024-04-02 16:34:35 +0200

    feat(reportController): feat get employer wtr and public holidays

--- src/main/java/com/absencia/diginamic/controller/ReportController.java
+++ src/main/java/com/absencia/diginamic/controller/ReportController.java
@@ -1,25 +1,41 @@
 package com.absencia.diginamic.controller;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
+import com.absencia.diginamic.entity.EmployerWtr;
+import com.absencia.diginamic.entity.PublicHoliday;
+import com.absencia.diginamic.service.EmployerWtrService;
+import com.absencia.diginamic.service.PublicHolidayService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/reports")
 public class ReportController {
-	public ReportController() {
-		//
+	private final PublicHolidayService publicHolidayService;
+	private final EmployerWtrService employerWtrService;
+
+	public ReportController(final PublicHolidayService publicHolidayService, final EmployerWtrService employerWtrService) {
+		this.publicHolidayService = publicHolidayService;
+		this.employerWtrService = employerWtrService;
 	}
 
 	@GetMapping("/employer-wtr-and-public-holidays")
-	public ResponseEntity<?> getEmployerWtrAndPublicHolidayReport() {
-		// TODO
+	public ResponseEntity<?> getEmployerWtrAndPublicHolidayReport(@RequestParam int year) {
+		List<EmployerWtr> employerWtr = employerWtrService.findByYear(year);
+		List<PublicHoliday> publicHolidays = publicHolidayService.findByYear(year);
 
-		return ResponseEntity.ok(Map.of("message", "TODO"));
+		Map<String, Object> response = new HashMap<>();
+		response.put("employerWtr", employerWtr);
+		response.put("publicHolidays", publicHolidays);
+
+		return ResponseEntity.ok(response);
 	}
 
 	@GetMapping("/planning")
